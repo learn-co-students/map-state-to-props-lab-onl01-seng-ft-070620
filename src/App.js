5cdc225fd308a216b81a4ce4c6f98102d8318e24
Ethan <eleetyson@gmail.com>
2020-11-16 15:08:00 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react'
 import UserInput from './components/UserInput'
-import ConnectedUsers from './components/Users'
+import Users from './components/Users'
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div className="App">
         <UserInput />
-        <ConnectedUsers />
+        <Users />
       </div>
-    );
+    )
   }
-}
 
-export default App
+}
